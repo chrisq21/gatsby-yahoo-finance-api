61e2d35275b0cc6559e03379ad5def959b220063
Chris Queen <chrisqueen10@gmail.com>
2020-05-06 14:49:18 -0400

    Moved notes to index.js and out of gatsby-node

--- index.js
+++ index.js
@@ -35,5 +35,15 @@ Notes
 * * Region: String (options: (AU | CA | FR | DE | HK | US | IT | ES | GB | IN))
 * * lang: String (options:  (en | fr | de | it | es | zh))
 
+// 1. Loop through all queries in pluginOptions
+// 2. Ensure that the required values are filled out for each query
+// 3. Throw errors if required fields are missing
+// 4. Initiate each query
+// 5. Catch query error, and throw customized error
+// 6. Create  nodes from the response using a shared helper function
+// 7. Display errors if found
+// 8. Convert to Typescript
+// 9. Add README
+
 
 */
